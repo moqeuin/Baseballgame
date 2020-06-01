5bb28f517ba27ed63a3e8c6736ce42d3acd9015f
moqeuin <qudrb414@naver.com>
2020-06-01 20:18:44 +0900

    baseballupdate

---
+++ src/baseBall/mainClass.java
@@ -0,0 +1,13 @@
+package baseBall;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		boolean clear = false;
+		
+		baseBall bb = new baseBall();
+		bb.init();
+		bb.loop();
+	}
+
+}
